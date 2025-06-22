0bdf63ad89c3790bed723a1b89c416259f0e5612
Eric Ayers <ericzundel@gmail.com>
2025-06-22 06:26:31 -0400

    Ran rubocop on another directory from within VSCode

--- classes_and_objects_part1/my_car.rb
+++ classes_and_objects_part1/my_car.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # See https://launchschool.com/books/oo_ruby/read/classes_and_objects_part1
 #
 class MyCar
@@ -11,19 +13,19 @@ class MyCar
     @speed = 0
   end
 
-  def increase_speed(n)
-    @speed += n
+  def increase_speed(value)
+    @speed += value
   end
 
-  def stop()
+  def stop
     @speed = 0
   end
 
-  def decrease_speed(n)
-    @speed -= n
-    if @speed < 0
-      self.stop()
-    end
+  def decrease_speed(value)
+    @speed -= value
+    return unless @speed.negative?
+
+    stop
   end
 
   def spray_paint(color)
