b6c232454c3eb68e99d55b3d2c144572df68a8e7
Eric Ayers <ericzundel@gmail.com>
2025-06-26 16:49:46 -0400

    fixed up some whining from rubocop

--- tic_tac_toe/spec/board_spec.rb
+++ tic_tac_toe/spec/board_spec.rb
@@ -3,11 +3,12 @@
 require 'curses'
 require_relative '../lib/board'
 
-describe 'Board tests' do
+describe Board do
   let(:mock_subwin) do
     instance_double(Curses::Window,
                     setpos: nil,
-                    addch: nil)
+                    addch: nil,
+                    refresh: nil)
   end
 
   let(:mock_window) do
@@ -21,35 +22,31 @@ describe 'Board tests' do
                     subwin: mock_subwin)
   end
 
-  before do
-    allow(Curses).to receive(:init_screen)
-    allow(Curses).to receive(:close_screen)
-    allow(Curses::Window).to receive(:new).and_return(mock_window)
-    allow(Curses::Window).to receive(:subwin).and_return(mock_window)
-  end
-
   let(:mock_screen) do
     instance_double(Screen,
                     main_win: mock_window,
                     side_size: 30)
   end
 
-  def board_maker
+  let(:board) do
     Board.new(mock_screen)
   end
 
-  context 'initialization tests' do
+  before do
+    allow(Curses).to receive(:init_screen)
+    allow(Curses).to receive(:close_screen)
+    allow(Curses::Window).to receive_messages({ new: mock_window, subwin: mock_window })
+  end
+
+  context 'when freshly initialized' do
     # Should be a fake
 
-    it 'should be initialized' do
-      board = board_maker
+    it 'is initialized' do
       expect(board.winner).to eq(Board::IN_PROGRESS)
     end
 
     it 'x winner horiz' do
-      board = board_maker
       board.set_tile_x(0, 0)
-      expect(board.winner).to eq(Board::IN_PROGRESS)
       board.set_tile_x(0, 1)
       expect(board.winner).to eq(Board::IN_PROGRESS)
       board.set_tile_x(0, 2)
@@ -57,9 +54,7 @@ describe 'Board tests' do
     end
 
     it 'o winner vert' do
-      board = board_maker
       board.set_tile_o(0, 2)
-      expect(board.winner).to eq(Board::IN_PROGRESS)
       board.set_tile_o(1, 2)
       expect(board.winner).to eq(Board::IN_PROGRESS)
       board.set_tile_o(2, 2)
@@ -67,9 +62,7 @@ describe 'Board tests' do
     end
 
     it 'o winner diag' do
-      board = board_maker
       board.set_tile_o(0, 0)
-      expect(board.winner).to eq(Board::IN_PROGRESS)
       board.set_tile_o(1, 1)
       expect(board.winner).to eq(Board::IN_PROGRESS)
       board.set_tile_o(2, 2)
@@ -77,9 +70,7 @@ describe 'Board tests' do
     end
 
     it 'x winner diag' do
-      board = board_maker
       board.set_tile_x(2, 0)
-      expect(board.winner).to eq(Board::IN_PROGRESS)
       board.set_tile_x(1, 1)
       expect(board.winner).to eq(Board::IN_PROGRESS)
       board.set_tile_x(0, 2)
